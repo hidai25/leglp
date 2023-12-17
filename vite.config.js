5563ce737af7834dab38dcba50c1e83c00cfdf19
Hidai Bar-Mor <hidai25@gmail.com>
2023-12-18 00:08:50 +0200

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/hidai25/leglp/', // Replace <REPO_NAME> with your GitHub repository name
+  base: '/leglp/', // Replace <REPO_NAME> with your GitHub repository name
 });
