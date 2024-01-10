fb7841b64307c1abdedc96a59ddff5e46c5d35d1
Hidai Bar-Mor <hidai25@gmail.com>
2024-01-10 22:45:56 +0200

    fixed consent problem

--- src/components/Analytics/Analytics.jsx
+++ src/components/Analytics/Analytics.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 const Analytics = () => {
+  // Define a regular function here to handle gtag
+  function gtag() {
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push(arguments);
+  }
+
   return (
     <Helmet>
-      {/* Paste your Google Analytics script here */}
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-HBJNKQ9VEK"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-
-  gtag('config', 'G-HBJNKQ9VEK');
-</script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() { dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', 'G-HBJNKQ9VEK');
+        `}
+      </script>
     </Helmet>
   );
 };
